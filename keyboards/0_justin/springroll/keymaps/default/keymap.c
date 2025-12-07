09ff4460683e45bedcc1a56522f52d78a6dc60f7
Justin Lam <justinmklam@gmail.com>
2025-12-07 08:36:57 -0800

    Add oled display

--- keyboards/0_justin/springroll/keymaps/default/keymap.c
+++ keyboards/0_justin/springroll/keymaps/default/keymap.c
@@ -22,3 +22,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,          KC_BSPC, KC_SPC,  KC_DEL,           KC_ENT,  KC_RALT, KC_APP,  KC_RCTL
     )
 };
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("FN"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("ADJ"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR(" NUM") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR(" CAP") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR(" SCR") : PSTR("    "), false);
+
+    // WPM counter
+    oled_write_P(PSTR("\nWPM: "), false);
+#ifdef WPM_ENABLE
+    oled_write(get_u8_str(get_current_wpm(), ' '), false);
+#else
+    oled_write_P(PSTR("N/A"), false);
+#endif
+
+    return false;
+}
+#endif
--- keyboards/0_justin/springroll/keymaps/vial/keymap.c
+++ keyboards/0_justin/springroll/keymaps/vial/keymap.c
@@ -19,6 +19,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL,          KC_BSPC, KC_SPC,  KC_DEL,           KC_ENT,  KC_RALT, KC_APP,  KC_RCTL
+        KC_LCTL,          KC_BSPC, KC_SPC,  KC_DEL,  KC_ENT,  KC_RALT, KC_LGUI,           KC_RCTL
     )
 };
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+}
+
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Spring Roll\n"), false);
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case 0:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case 1:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case 2:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(PSTR("LED: "), false);
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR") : PSTR("   "), false);
+
+    // WPM counter
+    oled_write_P(PSTR("\nWPM: "), false);
+#ifdef WPM_ENABLE
+    oled_write(get_u8_str(get_current_wpm(), ' '), false);
+#else
+    oled_write_P(PSTR("N/A"), false);
+#endif
+
+    return false;
+}
+#endif
